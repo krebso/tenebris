997fbe8d9d568de1e841e69f1fbced2786a12185
Martin Krebs <martin.krebs@kiwi.com>
2023-11-11 13:47:24 +0100

    Restore master

--- tenebris/domain/interfaces/method.py
+++ tenebris/domain/interfaces/method.py
@@ -1,9 +1,6 @@
 from abc import ABCMeta, abstractmethod
-from typing import Callable
 
-import numpy as np
 import torch
-from pytorch_grad_cam.utils.model_targets import ClassifierOutputTarget
 from torch import Tensor
 from torch.nn import Module
 
@@ -13,22 +10,11 @@ class ExplainabilityMethod(metaclass=ABCMeta):
 
     @abstractmethod
     def model(self) -> Module:
-        """
-        Returns:
-             Model the method is using
-        """
+        """Return model the method is using"""
 
     @abstractmethod
-    def _attribute_tensor(self, input_: Tensor, target: int | Tensor) -> Tensor:
-        """Generates explanation for given model and input
-
-        Args:
-            input_: torch Tensor
-            target: with respect to what we compute the attribution
-
-        Returns:
-            Tensor of explanations
-        """
+    def _attribute_tensor(self, input_: Tensor, target: Tensor) -> Tensor:
+        """Generates explanation for model given input and target"""
 
     def attribute(self, input_: Tensor | tuple[Tensor, ...], target: int | Tensor) -> Tensor:
         if isinstance(input_, tuple):
@@ -36,18 +22,12 @@ class ExplainabilityMethod(metaclass=ABCMeta):
             assert all(map(lambda t: len(t.size()) == 3, input_))  # C x W x H
             input_ = torch.stack(input_)
 
-        batch_size = torch.size(input_)[0]
-
+        batch_size = input_.size()[0]
         if isinstance(target, int):
-            target = torch.stack((target for _ in range(batch_size)))
+            target = torch.stack([torch.tensor(target) for _ in range(batch_size)])
+
+        if (n_target := target.size()[0]) != batch_size:
+            assert n_target == 1
+            target = torch.stack([target for _ in range(batch_size)])
 
         return self._attribute_tensor(input_, target)
-
-
-def pytorch_gradcam_format_explanation(f: Callable[..., np.ndarray]) -> Callable[..., Tensor]:
-    # add lost channel to BW images
-    # assert this is only when it is indeed bw image kekw
-    def _format_tensor(*args, **kwargs) -> Tensor:
-        return torch.from_numpy(f(*args, **kwargs)).unsqueeze(1)
-
-    return _format_tensor
--- tenebris/domain/interfaces/metric.py
+++ tenebris/domain/interfaces/metric.py
@@ -1,12 +1,9 @@
 from abc import ABCMeta, abstractmethod
 from enum import Enum
-from typing import TypeVar
 
 from torch import Tensor
 
 from tenebris.domain.interfaces.method import ExplainabilityMethod
-
-T = TypeVar("T")
 
 
 class ReduceStrategy(Enum):
@@ -19,26 +16,14 @@ class Metric(metaclass=ABCMeta):
     reduce_strategy: ReduceStrategy
 
     @abstractmethod
-    def _compute(self, method: ExplainabilityMethod, input_: Tensor, target: int | Tensor) -> T:
-        ...
+    def _compute(self, method: ExplainabilityMethod, input_: Tensor, target: int | Tensor) -> float:
+        """Compute the metric for method"""
+
 
     def compute(self, methods: list[ExplainabilityMethod], input_: Tensor, target: int | Tensor) -> dict:
-        """Runs the benchmark and computes the relevant metric for each method
-
-        Args:
-            methods: list of methods for which the metric is calculated
-            input_: input tensor which is passed to the methods
-            target: class with respect to which we compute the attributions
-
-        Returns:
-            A dict mapping method name to computed metric value
-        """
-
         metric = {}
 
         for method in methods:
-            input_.requires_grad_(True)
             metric[method.name] = self._compute(method, input_, target)
-            input_.requires_grad_(False)
 
         return metric
