b7cdc997aea02d778dd5f6c9b098a3c41b8c4c3e
Martin Krebs <martin.krebs@kiwi.com>
2023-11-11 16:35:38 +0100

    Checkout changes

---
+++ tenebris/domain/metrics/controlled_experiment.py
@@ -0,0 +1,17 @@
+from torch import Tensor
+
+from tenebris.domain.interfaces.method import ExplainabilityMethod
+from tenebris.domain.interfaces.metric import Metric, ReduceStrategy
+from tenebris.domain.tensors.functions import jaccard_similarity, positive_attribution_mask
+
+
+class ControlledExperiment(Metric):
+    name = "ControlledExperiment"
+    reduce_strategy = ReduceStrategy.AVERAGE
+
+    def _compute(self, method: ExplainabilityMethod, input_: Tensor, target: int | Tensor, **kwargs) -> float:
+        annotation = kwargs["annotation"]
+        synthetic_input = input_ * annotation
+        binary_synthetic_explanation = positive_attribution_mask(method.attribute(synthetic_input, target))
+
+        return jaccard_similarity(annotation, binary_synthetic_explanation)
---
+++ tenebris/domain/metrics/infidelity.py
@@ -0,0 +1,27 @@
+from typing import Callable
+
+from captum.metrics import infidelity
+from torch import Tensor
+
+from tenebris.domain.interfaces.method import ExplainabilityMethod
+from tenebris.domain.interfaces.metric import Metric, ReduceStrategy
+
+
+class Infidelity(Metric):
+    name = "Infidelity"
+    reduce_strategy = ReduceStrategy.AVERAGE
+
+    def __init__(self, perturbation_fn: Callable[..., Tensor], baselines: int | Tensor) -> None:
+        self._perturbation_fn = perturbation_fn
+        self._baselines = baselines
+
+    def _compute(self, method: ExplainabilityMethod, input_: Tensor, target: int | Tensor, **kwargs) -> bool:
+        attribution = method.attribute(input_, target)
+        return infidelity(
+            forward_func=method.model(),
+            perturb_func=self._perturbation_fn,
+            inputs=input_,
+            attributions=attribution,
+            baselines=self._baselines,
+            target=target,
+        ).item()
